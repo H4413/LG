462add17e82b2b9971bb3166a837cd1652c7b9ba
Cgg <clement.geiger@gmail.com>
2011-03-14 14:53:03 +0100

    +check command line, recup fichier dtd wip

--- Realisation/AXD/main.cpp
+++ Realisation/AXD/main.cpp
@@ -1,12 +1,53 @@
+// ### INCLUDE & NAMESPACES ###
+
+// system includes
 #include <iostream>
+#include <cstdio>
+#include <string>
+
+// projet includes
+// nothing yet
 
 using namespace std;
 
+
+// ### CONSTANT DEFINITIONS ###
+
+
+// ### LOCAL FUNCTIONS PROTOTYPE ###
+
+
+// ### LOCAL FUNCTIONS IMPLEMENTATIONS ###
+
+/**************************************************************************/
+/*!
+***************************************************************************/
 int main
 (
-    void
+    int          argn,
+    char * const argv[]
 )
 {
+    // Check the input command line
+    if( argn != 2 )
+    {
+	cerr << "usage : ./axd filename.xml" << endl;
+
+	return -1;
+    }
+
+    // Retrieve the dtd's filename
+    FILE * pXmlFile = fopen( argv[ 1 ], "r" );
+
+    if( pXmlFile == NULL )
+    {
+	cerr << "fichier spécifié introuvable" << endl;
+	
+	return -1;
+    }
+
+    fclose( pXmlFile );
+
     cout << "hello\n" << endl;
 
     return 0;
