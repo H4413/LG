00e7bd322b63c759639405b333cb7f096ed6c19e
Raphaël Lizé <raphael.lize@gmail.com>
2011-03-16 10:12:00 +0100

    * Xml.[h,cpp]

--- Realisation/AnalyseurXML/Xml.cpp
+++ Realisation/AnalyseurXML/Xml.cpp
@@ -10,12 +10,19 @@
 
 /* XmlElement */
 
-//XmlElement::
+void XmlElement::AddNode( XmlNode node )
 {
+    nodeList.push_back( node );
 }
 
-//XmlElement::
+void XmlElement::AddAttribute( XmlAtt att )
 {
+    attList.push_back( att );
+}
+
+void XmlElement::AddAttribute( string n, string v )
+{
+    attList.push_back( XmlAtt att(n, v) );
 }
 
 /* XmlContent */
