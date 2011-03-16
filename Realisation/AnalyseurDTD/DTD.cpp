f340b55e45442ddb1813b82cf6b2a588604e6c6d
Victor Borges <victorborgesfg@gmail.com>
2011-03-16 10:45:45 +0100

    DTD

--- Realisation/AnalyseurDTD/DTD.cpp
+++ Realisation/AnalyseurDTD/DTD.cpp
@@ -1,1 +1,51 @@
 /* Implémentation de la DTD */ 
+
+#include "DTD.h"
+
+#include <iostream>
+
+/************************** DTDDocument ******************************/
+void DTDDocument::Display() const
+{
+	cout << "<!DOCTYPE " << name << " [ " << endl << endl;
+	map <string, DTDElement>::const_iterator it;
+	for (it = elements.begin(); it != elements.end(); ++it)
+		it->second.Display();
+	cout << endl << "]>";
+}
+
+/************************** DTDElement ******************************/
+void DTDElement::Display() const
+{
+	cout << "<!ELEMENT " << name << " ";
+	vector <DTDContentspec*>::const_iterator it;
+	for (it = contentspec.begin(); it != contentspec.end(); ++it)
+		(*it)->Display();
+	cout << ">" << endl;
+}
+
+/************************** DTDSequence ******************************/
+void DTDSequence::Display() const
+{
+	vector <DTDChildren*>::const_iterator it;
+	cout << "(";
+	for (it = seq.begin(); it != seq.end(); ++it)
+		(*it)->Display();
+	cout << ")";
+}
+
+/************************** DTDChoice ******************************/
+void DTDChoice::Display() const
+{
+	vector <DTDChildren*>::const_iterator it;
+	cout << "(";
+	for (it = choice.begin(); it != choice.end(); ++it)
+		(*it)->Display();
+	cout << ")";
+}
+
+/************************** DTDNom ******************************/
+void DTDNom::Display() const
+{
+	cout << name;
+}
